68a929abdd39bacc1c07e1b803c10bfe00bc9e4d
TarekAlhalabi <tarekalhalaby946@gmail.com>
2021-01-22 03:22:28 +0200

    improve the DBConnection class

--- php/DBConnection.class.php
+++ php/DBConnection.class.php
@@ -8,24 +8,35 @@
     with the mysql database
 */
 
-class db{
-    
+class DBConnection{
+
+    //database type
+    private $dbtype = "mysql";
+    //domain or host
     private $host = "localhost";
+    //database name
     private $dbname = "test";
+    //database username 
     private $username = "root";
+    //database user password
     private $password = "";
     private $conn;
 
     public function connect(){
         try{
-            $conn = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname, $this->username, $this->password);
+            //try to connect
+            $conn = new PDO($this->db_type.":host=".$this->host.";dbname=".$this->dbname, $this->username, $this->password);
+            //set the error reporting type to exceptions
             $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //set the default data fetch mode to associative array
             $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            $this->conn = $conn;
 
+            $this->conn = $conn;
+            //return the connection variable
             return $this->conn;
 
         }catch(PDOException $e){
+            //report the error message
             die("Failed to connect to database: " . $e->getMessage());
         }
     }
